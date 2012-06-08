62280e33258d748177ce48fbf4e5da1a404fe867
patonar <patonar@gmail.com>
2012-06-08 17:05:30 +0100

    Finished section 6.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :email
 
-	validates :name, presence: true
+
+
+	# Model Validations
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+	validates :name, presence: true, length: {maximum: 50}
+	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
+				uniqueness: { case_sensitive: false }
+
+	# Before Save
+	before_save { |user| user.email = email.downcase}
+
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe User do
 
-	before { @user = User.new(name: "Example User", email: "user@example.com")}
+	before do 
+
+		 @user = User.new(name: "Example User", email: "user@example.com")
+
+	end
 
 	subject {@user}
 
@@ -17,4 +21,54 @@ describe User do
 		before {@user.name = " "}
 		it {should_not be_valid}
 	end
+
+	#Email address validation
+
+	describe "When email address is not present" do
+		before {@user.email = ""}
+
+		it { should_not be_valid}
+	end
+
+	  describe "when email format is invalid" do
+	    it "should be invalid" do
+	      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+	      addresses.each do |invalid_address|
+	        @user.email = invalid_address
+	        @user.should_not be_valid
+	      end      
+	    end
+	  end
+
+	  describe "when email format is valid" do
+	    it "should be valid" do
+	      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+	      addresses.each do |valid_address|
+	        @user.email = valid_address
+	        @user.should be_valid
+	      end      
+	    end
+	  end
+
+
+
+	#Name length validation
+
+	describe "When name is too long" do
+		before {@user.name = "a" * 51 }
+
+		it {should_not be_valid}
+	end
+
+
+	describe "when email address is already taken" do
+		before do
+			user_with_same_email = @user.dup
+			user_with_same_email.email = @user.email.upcase
+			user_with_same_email.save
+		end
+
+		it {should_not be_valid}
+	end
+
 end
